b40a8605f11d7a7c49aec325f6a65051f2639c31
Panmaslak <73931821+Panmaslak@users.noreply.github.com>
2023-04-20 18:37:29 +0200

    Add files via upload

---
+++ SubjectService.java
@@ -0,0 +1,15 @@
+package com.example.application.service;
+
+import com.example.application.model.Subject;
+import com.example.application.repository.SubjectRepository;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SubjectService {
+
+    SubjectRepository subjectRepository;
+
+}
